fff55f4d9fec18d9d520a1685163483a29f5b35d
abdelilah-dev <contactme.abdelilah@gmail.com>
2026-02-14 15:27:58 +0100

    updated

--- module_00/ex5/ft_count_harvest_recursive.py
+++ module_00/ex5/ft_count_harvest_recursive.py
@@ -1,7 +1,7 @@
 def ft_count_harvest_recursive(total_day: int = None, day: int = 1) -> None:
     if total_day is None:
         total_day = int(input("Days until harvest: "))
-    if total_day < day:
+    if day > total_day:
         print("Harvest time!")
         return
     print(f"Day {day}")
